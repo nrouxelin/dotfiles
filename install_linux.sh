61ddc8013987f450c43526d8f962d5305bc73e5a
Nathan Rouxelin <nrouxelin@gmail.com>
2019-10-02 14:32:38 +0200

    add install script for linux

---
+++ install_linux.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+dir=$(pwd)
+
+# Files to consider
+files=".vimrc .tmux.conf .vim .bash_alias"
+
+backup_dir=~/old_dotfiles
+echo "Creating $backup_dir to backup any existing dotfile"
+mkdir -p $backup_dir
+
+for file in $files
+do
+  echo "Back-up existing dotfiles to $backup_dir"
+  mv ~/$file $backup_dir/
+  echo "Creating symlink to $file in the home directory"
+  ln -s $dir/$file ~/$file
+done
+
+echo "Sourcing bash_alias"
+echo "source ~/.bash_alias" >> ~/.bashrc
+
+echo "Sourcing new bash config"
+source ~/.bashrc
+
+# installing vim plug-ins
+cd ~/.vim
+git clone https://github.com/dracula/vim.git bundle/dracula
+git clone https://github.com/itchyny/lightline.vim.git bundle/lightline.vim
+
